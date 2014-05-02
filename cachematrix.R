50363abd38d8d33ae605fd56b0d77cf206ec2f83
istrah <igystrah@gmail.com>
2014-05-02 20:46:46 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
 ##This function takes a matrix, and sets a list containing several functions intended to cache te matrix  
 
 makeCacheMatrix <- function(x = matrix()) {
-## Return a matrix that is the inverse of 'x'
-        m<-NULL
+        ## creates a variable for caching a matrix x
+        cachedMatrix<-NULL
+        
+        ## creates function to store x and empties cache variable
         set<- function(y){
                 x<<-y
-                m<<-NULL
+                cachedMatrix<<-NULL
         }
+        
+        ## crates function to return x
         get<-function()x
-        setsolve<-function(solv) m <<- solv
-        getsolve<-function()m
+        
+        ##creates a function for caching solve(x)
+        setsolve<-function(solv) cachedMatrix <<- solv
+        
+        ## creates a function to return solve(x)
+        getsolve<-function()cachedMatrix
+        
+        ##creates a list of above function
         list(set=set,get=get,
              setsolve=setsolve,
              getsolve=getsolve)
@@ -20,14 +30,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function takes a cached values from above function (if they are set), inverses it and returns inversed matrix
 
 cacheSolve <- function(x, ...) {
-      ## Return a matrix that is the inverse of 'x'
-        m <- x$getsolve()
-        if(!is.null(m)) {
+      ##checks if cache is set and if it is stores it
+        cachedMatrix <- x$getsolve()
+        if(!is.null(cachedMatrix)) {
                 message("getting cached data")
-                return(m)
+                return(cachedMatrix)
         }
+        
+        ## stores return from funciton get()
         data <- x$get()
-        m <- solve(data, ...)
-        x$setsolve(m)
-        m
+        
+        ## uses a function solve() to inverse cahced matrix x
+        solv <- solve(data, ...)
+        
+        ##stores cahce for set solve
+        x$setsolve(solv)
+        
+        ##returns solve
+        solv
 }
